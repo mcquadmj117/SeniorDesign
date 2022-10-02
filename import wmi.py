6911f84a84ccdd6ff3e282e17201e554a3d0c247
mcquadmj <mmcquad.17@gmail.com>
2022-10-02 11:32:27 -0400

    Listing current processes before listening to new ones
    Listing current processes before listening to new ones

--- import wmi.py
+++ import wmi.py
@@ -1,6 +1,9 @@
 import wmi
 c = wmi.WMI()
+
+for process in c.Win32_Process():
+  print({process.Name})
 process_watcher = c.Win32_Process.watch_for("creation")
 while True:
   new_process = process_watcher()
-  print(new_process.Caption)
+  print({new_process.Caption})
